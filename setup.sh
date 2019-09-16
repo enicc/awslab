8ee61ccbb9d4e545d053fc76a758a2e380496c0a
Ewan <ewann@users.noreply.github.com>
2019-09-16 18:50:21 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo curl --silent --location -o /usr/local/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/v1.13.7/bin/linux/amd64/kubectl
+
+
+sudo chmod +x /usr/local/bin/kubectl
+
+sudo apt install -y jq gettext
+
+for command in kubectl jq envsubst
+  do
+    which $command &>/dev/null && echo "$command in path" || echo "$command NOT FOUND"
+  done
+  
+#https://eksworkshop.com/prerequisites/iamrole/
